129e38becd5a66d91ffe88da96d31e3c023ecdd8
Hans Nowak <zephyrfalcon@gmail.com>
2016-07-13 16:18:24 -0400

    use requests library

--- grab_neoseeker.py
+++ grab_neoseeker.py
@@ -8,34 +8,18 @@ import urllib.request
 from urllib.parse import urlparse
 #
 from bs4 import BeautifulSoup as BS
+import requests
 
 class Options:
     debug = False
 
-def fetch_url(url, origin):
-    headers = {
-        #"Host": "www.neoseeker.com", 
-        "Referer": origin, 
-        #"User-Agent": USER_AGENT,
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        #"Accept-Encoding": "gzip, deflate, sdch",
-        "Accept-Language": "en-US,en;q=0.8,de;q=0.6,nl;q=0.4",
-        "Cache-Control": "max-age=0",
-        "Connection": "keep-alive",
-        # XXX cookie might need to be replaced
-        "Cookie": "ns=1t2ilpdrkr49s1tbl9rgp801b5;_gaost=.nv=1.r=www_d_google_d_com.rk=null;_gaos=.gaos_r=www_d_google_d_com.mc=(no)|(no)|(no).gaos_k=null.pc=1;_nrlsk=nrlsk_c=3.et=1466645187; _gat=1;_ga=GA1.2.389298541.1466645409",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-    }
-    #print("Fetching:", url, "with origin:", origin)
-    req = urllib.request.Request(url, headers=headers, origin_req_host=origin)
-    u = urllib.request.urlopen(req)
-    data = u.read() # assume it's small enough that we can read it all at once
-    return data
 
 def filename_from_url(url):
     url_parts = urlparse(url)
     return os.path.basename(url_parts.path)
+
+# dodgy default cookie. may need to be changed
+DEFAULT_COOKIE = "ns=1t2ilpdrkr49s1tbl9rgp801b5;_gaost=.nv=1.r=www_d_google_d_com.rk=null;_gaos=.gaos_r=www_d_google_d_com.mc=(no)|(no)|(no).gaos_k=null.pc=1;_nrlsk=nrlsk_c=3.et=1466645187; _gat=1;_ga=GA1.2.389298541.1466645409"
 
 
 class NeoSeekerGrabber:
@@ -50,9 +34,31 @@ class NeoSeekerGrabber:
         if not os.path.exists(self.dirname):
             os.makedirs(self.dirname)
 
+    def fetch_url(self, url, origin):
+        headers = {
+            "Referer": origin, 
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            #"Accept-Encoding": "gzip, deflate, sdch",
+            "Accept-Language": "en-US,en;q=0.8,de;q=0.6,nl;q=0.4",
+            "Cache-Control": "max-age=0",
+            "Connection": "keep-alive",
+            "Cookie": DEFAULT_COOKIE, 
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+        }
+        #print("Fetching:", url, "with origin:", origin)
+        '''
+        req = urllib.request.Request(url, headers=headers) 
+        u = urllib.request.urlopen(req)
+        data = u.read() # assume it's small enough that we can read it all at once
+        '''
+        r = requests.get(url, headers=headers)
+        data = r.content # get binary content; ignore encoding
+        return data
+    
     def grab_faqs(self, url):
         print("Grabbing:", url, "...")
-        faq_source = fetch_url(url, url)
+        faq_source = self.fetch_url(url, url)
 
         if self.options.debug:
             print("#DEBUG: Writing FAQ source file...")
@@ -67,7 +73,7 @@ class NeoSeekerGrabber:
     def grab_faq(self, link, origin):
         print("Grabbing:", link, "...")
         url = link['href']
-        data = fetch_url(url, origin)
+        data = self.fetch_url(url, origin)
         print(len(data), "bytes")
 
         if self.options.debug:
@@ -85,7 +91,7 @@ class NeoSeekerGrabber:
             resource = url # store the HTML page as-is
             src_data = data
         else:
-            src_data = fetch_url(resource, url) # raw data
+            src_data = self.fetch_url(resource, url) # raw data
         basename = filename_from_url(resource)
         out_path = os.path.join(self.dirname, basename)
         print("Writing:", basename, "...")
@@ -160,7 +166,7 @@ def determine_dir_name(url):
 
 if __name__ == "__main__":
 
-    opts, args = getopt.getopt(sys.argv[1:], "")
+    opts, args = getopt.getopt(sys.argv[1:], "d")
     url = args[0]
     assert "/faqs" in url, "This does not look like a NeoSeeker FAQs page."
     if args[1:]:
@@ -171,7 +177,11 @@ if __name__ == "__main__":
         print("Storing in directory:", dirname)
 
     options = Options()
-    # TODO: set options from command line arguments...
+
+    # set options from command line arguments...
+    for o, a in opts:
+        if o == "-d":
+            options.debug = True
 
     grabber = NeoSeekerGrabber(url, dirname, options)
 
